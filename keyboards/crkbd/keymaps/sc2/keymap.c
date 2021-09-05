a8214e35001b2a774462400049e9baa853cb37d1
Daniel R <danramteke>
2021-09-05 00:12:45 -0400

    swap ctrl and lower

--- keyboards/crkbd/keymaps/sc2/keymap.c
+++ keyboards/crkbd/keymaps/sc2/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,                          KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,     KC_G,                          KC_H,     KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
 KC_SPACE,   KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,                          KC_N,     KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                                    KC_LSFT, U_LOWER, KC_LCTL,     KC_ENT,  U_RAISE,  KC_LALT
+                                    KC_LSFT, KC_LCTL, U_LOWER,      KC_ENT,  U_RAISE,  KC_LALT
   ),
 
 
