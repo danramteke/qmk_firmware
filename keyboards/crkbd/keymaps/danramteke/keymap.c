69050e277ae8d448d499c8800e6f35a8a94a2978
Full Queue Developer <fullqueuedeveloper@fullqueuedeveloper.com>
2022-10-05 20:52:34 -0400

    gui in corner

--- keyboards/crkbd/keymaps/danramteke/keymap.c
+++ keyboards/crkbd/keymaps/danramteke/keymap.c
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLMK] = LAYOUT( \
       KC_ESC,   KC_Q,   KC_W,    KC_F,    KC_P,     KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_QUOT,  KC_MUTE,
       KC_TAB,   KC_A,   KC_R,    KC_S,    KC_T,     KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,  KC_VOLU,
-      KC_SPC,   KC_Z,   KC_X,    KC_C,    KC_D,     KC_V,                      KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_VOLD,
+     KC_LGUI,   KC_Z,   KC_X,    KC_C,    KC_D,     KC_V,                      KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_VOLD,
                                       KC_LCTRL,  KC_LSFT, U_LOWER, U_RAISE,  KC_SPC,  KC_ENT
   ),
 
